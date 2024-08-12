217bbe11355537f9041454f1908a0a1cac17ed8b
Henrik Beige <henrik.beige@deutschebahn.com>
2024-08-12 16:19:41 +0200

    maven-publish integration - publishToMavenLocal already works perfectly

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    `maven-publish`
 }
 
 group = "de.db.moredux"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -20,4 +21,54 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "MoRedux"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name = artifactId
+                description = "A Redux library in Kotlin to manage UI state and react on state changes"
+                url = "https://github.com/dbvertrieb/moredux"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "hbeige"
+                        name = "Henrik Beige"
+                        email = "henrik.beige@deutschebahn.com"
+                    }
+                }
+                scm {
+                    url = "https://github.com/dbvertrieb/MoRedux"
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri(layout.buildDirectory.dir("repos/releases"))
+            val snapshotsRepoUrl = uri(layout.buildDirectory.dir("repos/snapshots"))
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+    }
 }
