ad4a876b7add04ab1200e18787be3ee9c36b7e36
Henrik Beige <henrik.beige@deutschebahn.com>
2024-08-20 08:00:57 +0200

    first try to switch to the vanniktech publish plugin, because gradle maven-publish is not capable of publishing to Sonatype portal yet

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
-import java.net.URI
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     kotlin("jvm") version libs.versions.kotlin
-    `maven-publish`
     signing
-    id("pl.allegro.tech.build.axion-release") version libs.versions.axion
+    alias(libs.plugins.vanniktech.publish)
+    alias(libs.plugins.axion)
 }
 
 // Axion plugin settings
@@ -44,71 +44,60 @@ java {
 }
 
 publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            artifactId = "moredux"
-            from(components["java"])
-            versionMapping {
-                usage("java-api") {
-                    fromResolutionOf("runtimeClasspath")
-                }
-                usage("java-runtime") {
-                    fromResolutionResult()
-                }
-            }
-            pom {
-                name = artifactId
-                description = "A Redux library in Kotlin to manage UI state and react on state changes"
-                url = "https://github.com/dbvertrieb/moredux"
-                licenses {
-                    license {
-                        name = "The Apache License, Version 2.0"
-                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "hbeige"
-                        name = "Henrik Beige"
-                        email = "henrik.beige@deutschebahn.com"
-                    }
-                }
-                scm {
-                    url = "https://github.com/dbvertrieb/MoRedux"
-                }
-            }
-        }
-    }
     repositories {
         maven {
-            name = "GitHubPackages"
-            url = URI("https://maven.pkg.github.com/dbvertrieb/MoRedux")
+            name = "githubPackages"
+            url = uri("https://maven.pkg.github.com/dbvertrieb/MoRedux")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
             }
         }
-        maven {
-            name = "OSSRH"
-            url = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
-            }
-        }
     }
 }
 
-tasks.withType<Sign>().configureEach {
-    // Skip signing completely, if the current build is not a release version
-    onlyIf("isReleaseVersion is set") { project.extra["isReleaseVersion"] as Boolean }
-}
+mavenPublishing {
+    // See https://vanniktech.github.io/gradle-maven-publish-plugin/central/ for documentation of vanniktech maven-publish plugin
 
-signing {
-    val signingKey: String? by project
-    val signingPassword: String? by project
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications["mavenJava"])
+    // Publishing to https://central.sonatype.com/
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Only apply signing when it's a release build
+    if (project.extra["isReleaseVersion"] as Boolean) {
+        signAllPublications()
+    }
+
+    val groupId = group.toString()
+    val artifactId = "moredux"
+    val version = scmVersion.version
+
+    coordinates(groupId, artifactId, version)
+
+    pom {
+        name.set("MoRedux")
+        description.set("A Redux inspired library in Kotlin to manage UI state and react on state changes")
+        inceptionYear.set("2024")
+        url.set("https://github.com/dbvertrieb/moredux")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id = "hbeige"
+                name = "Henrik Beige"
+                email = "henrik.beige@deutschebahn.com"
+            }
+        }
+        scm {
+            url = "https://github.com/dbvertrieb/moredux"
+            connection.set("scm:git:git://github.com/dbvertrieb/moredux.git")
+            developerConnection.set("scm:git:ssh://git@github.com/dbvertrieb/moredux.git")
+        }
+    }
 }
 
 tasks.javadoc {
